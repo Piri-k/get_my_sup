a4f193afebde44700d822da5900f6656283cc7c3
jean lafore <jean.l94240@gmail.com>
2023-02-27 15:36:08 +0100

    creation controller services

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,51 @@
+class ServicesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :show, :index ]
+  before_action :set_service, only: [ :show, :edit, :upgrade, :destroy]
+
+  def index
+    @services = Service.all
+  end
+
+  def new
+    @service = Service.new
+  end
+
+  def create
+    @service = Service.new(service_params)
+    if @service.save
+      redirect_to root_path(@service)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def upgrade
+    @service.update(service_params)
+    if @service.save
+      redirect_to root_path(@service)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @service.destroy
+    redirect_to root_path(@service), status: :see_other
+  end
+
+  private
+
+  def service_params
+    params.require(:service).permit(:title, :content, :price_day, :location)
+  end
+
+  def set_service
+    @service = Service.find(params[:id])
+  end
+end
