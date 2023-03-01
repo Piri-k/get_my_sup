3c0923f89dd3150e1c6a0460c6f8355e3fe3c5c0
Guillaume Laurent <laurenaute@gmail.com>
2023-03-01 12:37:52 +0100

    fixed models validations and updated new and edit user view

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,10 +2,5 @@ class Reservation < ApplicationRecord
   belongs_to :service
   belongs_to :user
 
-  validates :start_at, presence: true
-  validates :end_at, presence: true
-  validates :message, presence: true
-  validates :address, presence: true
-  validates :service_id, presence: true
-  validates :user_id, presence: true
+  validates :start_at, :end_at, :message, :address, :service_id, :user_id, presence: true
 end
