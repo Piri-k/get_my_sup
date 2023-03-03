79a485a4bde10cebb0f3db692a5ed52215cd6a7c
jean lafore <jean.l94240@gmail.com>
2023-03-03 12:46:52 +0100

    ajout de la map dans la show du service

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,7 @@
 class Service < ApplicationRecord
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
+
   belongs_to :user
   has_many_attached :photos
   has_many :reservations
