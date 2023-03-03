50f055a75f6b708b535cab77ed6f987d937c49be
gauthier halphen <gauthier.halphen@gmail.com>
2023-03-03 15:05:04 +0100

    seed via chatgpt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_01_125507) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_03_130503) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -81,6 +81,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_125507) do
     t.datetime "remember_created_at"
     t.string "power"
     t.boolean "admin"
+    t.text "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
